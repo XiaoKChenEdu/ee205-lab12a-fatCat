80b1fe7c47f4857881af9d26742cab55b2876866
Xiaokang Chen <xiaokang.chen888@gmail.com>
2022-04-14 14:37:17 -1000

    Hello, World

--- classWeight.h
+++ classWeight.h
@@ -2,20 +2,9 @@
 ///         University of Hawaii, College of Engineering
 /// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
 ///
-/// @file main.h
+/// @file classWeight.h
 /// @version 1.0
 ///
 /// @author Xiaokang Chen <xiaokang@hawaii.edu>
 /// @date   14_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
-
-
-#include <iostream>
-
-
-int main() {
-
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-
-}
