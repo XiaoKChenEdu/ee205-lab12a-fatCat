1ec866c9d8b42d7a39105c482d1aad4612021879
Xiaokang Chen <xiaokang.chen888@gmail.com>
2022-04-15 20:44:11 -1000

    Finished Stubbing out function base on Data Structures reference.

--- classWeight.h
+++ classWeight.h
@@ -23,6 +23,7 @@ class classWeight {
         enum UnitOfWeight { POUND, KILO, SLUG };
 
     public:
+        //////////// Public Attributes ////////////
         static const float UNKNOWN_WEIGHT   ;
         static const float KILOS_IN_A_POUND ;
         static const float SLUGS_IN_A_POUND ;
@@ -30,6 +31,18 @@ class classWeight {
         static const string POUND_LABEL ;
         static const string KILO_LABEL  ;
         static const string SLUG_LABEL  ;
+        //////////// Public Attributes ////////////
+
+    private:
+        //////////// Private Attributes ////////////
+        bool bWeightIsKnown = false ;
+        bool bWeightHasMax  = false ;
+
+        enum UnitOfWeight unitOfWeight = POUND;
+
+        float weight    {} ;
+        float maxWeight {} ;
+        //////////// Private Attributes ////////////
 
     public:
         //////////// Constructors ////////////
@@ -50,23 +63,52 @@ class classWeight {
 
     public:
         //////////// Getters ////////////
-        bool isWeightKnow() const noexcept;
-        bool hasMaxWeight() const noexcept;
+        bool isWeightKnow() const noexcept ;
+        bool hasMaxWeight() const noexcept ;
 
-        float getWeight() const noexcept;
-        float getWeight( UnitOfWeight weightUnits ) const noexcept;
-        float getMaxWeight() noexcept;
-        UnitOfWeight getWeightUnit() const noexcept;
+        float getWeight    ()                           const noexcept ;
+        float getWeight    ( UnitOfWeight weightUnits ) const noexcept ;
+        float getMaxWeight ()                                 noexcept ;
+
+        UnitOfWeight getWeightUnit() const noexcept ;
         //////////// Getters ////////////
 
         //////////// Setters ////////////
-        void setWeight( float newWeight          );
+        void setWeight( float newWeight          ) ;
         void setWeight( float newWeight,
-                        UnitOfWeight weightUnits );
+                        UnitOfWeight weightUnits ) ;
         //////////// Setters ////////////
 
     private:
         //////////// Setters ////////////
-        void setMaxWeight( float newMaxWeight );
+        void setMaxWeight( float newMaxWeight ) ;
         //////////// Setters ////////////
+
+    public:
+        //////////// Conversion ////////////
+        static float fromKilogramToPound ( float kilogram           ) noexcept ;
+        static float fromPoundToKilogram ( float pound              ) noexcept ;
+        static float fromSlugToPound     ( float slug               ) noexcept ;
+        static float fromPoundToSlug     ( float pound              ) noexcept ;
+        static float convertWeight       ( float        fromWeight,
+                                           UnitOfWeight fromUnit,
+                                           UnitOfWeight toUnit      ) noexcept ;
+        //////////// Conversion ////////////
+
+    public:
+        //////////// Validation & Print ////////////
+        bool isWeightValid ( float checkWeight ) const noexcept ;
+        bool validate      ()                    const noexcept ;
+
+        void print() const noexcept ;
+        //////////// Validation & Print ////////////
+
+    public:
+        //////////// Operators ////////////
+        bool operator== ( const classWeight& rhs_Weight ) const ;
+        bool operator<  ( const classWeight& rhs_Weight ) const ;
+
+        classWeight& operator+=( float rhs_addToWeight ) ;
+        //////////// Operators ////////////
+
 };
