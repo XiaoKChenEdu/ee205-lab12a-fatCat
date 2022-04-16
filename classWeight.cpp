52a58d888304d47b6825eb3299627520fbb27140
Xiaokang Chen <xiaokang.chen888@gmail.com>
2022-04-15 19:09:44 -1000

    Stubbed out some functions.

--- classWeight.cpp
+++ classWeight.cpp
@@ -8,3 +8,94 @@
 /// @author Xiaokang Chen <xiaokang@hawaii.edu>
 /// @date   14_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
+
+
+#include "classWeight.h"
+
+
+/////////////// Attributes ///////////////
+const float UNKNOWN_WEIGHT   = -1       ;
+const float KILOS_IN_A_POUND = 0.453592 ;
+const float SLUGS_IN_A_POUND = 0.031081 ;
+
+const string POUND_LABEL = "Pound" ;
+const string KILO_LABEL  = "Kilo"  ;
+const string SLUG_LABEL  = "Slug"  ;
+/////////////// Attributes ///////////////
+
+
+
+//////////////////////////////////// Constructors ////////////////////////////////////
+classWeight::classWeight() {
+
+}
+
+classWeight::classWeight(float newWeight) {
+
+}
+
+classWeight::classWeight(classWeight::UnitOfWeight newUnitOfWeight) noexcept {
+
+}
+
+classWeight::classWeight(float newWeight, classWeight::UnitOfWeight newUnitOfWeight) {
+
+}
+
+classWeight::classWeight(float newWeight, float newMaxWeight) {
+
+}
+
+classWeight::classWeight(classWeight::UnitOfWeight newUnitOfWeight, float newMaxWeight) {
+
+}
+
+classWeight::classWeight(float newWeight, classWeight::UnitOfWeight newUnitOfWeight, float newMaxWeight) {
+
+}
+//////////////////////////////////// Constructors ////////////////////////////////////
+
+
+
+//////////////////////////////////// Getters ////////////////////////////////////
+bool classWeight::isWeightKnow() const noexcept {
+    return false;
+}
+
+bool classWeight::hasMaxWeight() const noexcept {
+    return false;
+}
+
+float classWeight::getWeight() const noexcept {
+    return 0;
+}
+
+float classWeight::getWeight(classWeight::UnitOfWeight weightUnits) const noexcept {
+    return 0;
+}
+
+float classWeight::getMaxWeight() noexcept {
+    return 0;
+}
+
+classWeight::UnitOfWeight classWeight::getWeightUnit() const noexcept {
+    return classWeight::POUND;
+}
+//////////////////////////////////// Getters ////////////////////////////////////
+
+
+
+//////////////////////////////////// Setters ////////////////////////////////////
+void classWeight::setWeight(float newWeight) {
+
+}
+
+void classWeight::setWeight(float newWeight, classWeight::UnitOfWeight weightUnits) {
+
+}
+
+//This is a private member function.
+void classWeight::setMaxWeight(float newMaxWeight) {
+
+}
+//////////////////////////////////// Setters ////////////////////////////////////
--- classWeight.h
+++ classWeight.h
@@ -8,3 +8,65 @@
 /// @author Xiaokang Chen <xiaokang@hawaii.edu>
 /// @date   14_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+
+class classWeight {
+
+    public:
+        enum UnitOfWeight { POUND, KILO, SLUG };
+
+    public:
+        static const float UNKNOWN_WEIGHT   ;
+        static const float KILOS_IN_A_POUND ;
+        static const float SLUGS_IN_A_POUND ;
+
+        static const string POUND_LABEL ;
+        static const string KILO_LABEL  ;
+        static const string SLUG_LABEL  ;
+
+    public:
+        //////////// Constructors ////////////
+        classWeight();
+
+        classWeight( float newWeight               )          ;
+        classWeight( UnitOfWeight newUnitOfWeight  ) noexcept ;
+        classWeight( float        newWeight,
+                     UnitOfWeight newUnitOfWeight  )          ;
+        classWeight( float newWeight,
+                     float newMaxWeight            )          ;
+        classWeight( UnitOfWeight newUnitOfWeight,
+                     float        newMaxWeight     )          ;
+        classWeight( float        newWeight,
+                     UnitOfWeight newUnitOfWeight,
+                     float        newMaxWeight     )          ;
+        //////////// Constructors ////////////
+
+    public:
+        //////////// Getters ////////////
+        bool isWeightKnow() const noexcept;
+        bool hasMaxWeight() const noexcept;
+
+        float getWeight() const noexcept;
+        float getWeight( UnitOfWeight weightUnits ) const noexcept;
+        float getMaxWeight() noexcept;
+        UnitOfWeight getWeightUnit() const noexcept;
+        //////////// Getters ////////////
+
+        //////////// Setters ////////////
+        void setWeight( float newWeight          );
+        void setWeight( float newWeight,
+                        UnitOfWeight weightUnits );
+        //////////// Setters ////////////
+
+    private:
+        //////////// Setters ////////////
+        void setMaxWeight( float newMaxWeight );
+        //////////// Setters ////////////
+};
